48a9ed7d4d1c8880c37c2318645f89093f809214
joe061005 <18233368@life.hkbu.edu.hk>
2021-05-21 21:12:46 +0800

    default props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import React, { Component } from 'react';
 
-function App() {
+// class App extends Component {
+//   state = {  }
+//   render() { 
+//     return (  
+//        <h1>Hello from a class</h1>   
+
+//     );
+//   }
+// } 
+ 
+// export default App;
+
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
     </div>
   );
 }
